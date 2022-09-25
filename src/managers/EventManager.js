a0d4d5b55632e2e0cd2e2d5cb71f1a5822f52523
Rose Cota <rosejcota@gmail.com>
2022-09-25 15:56:56 -0500

    add request methods for getSingleEvent and updateEvent

--- src/managers/EventManager.js
+++ src/managers/EventManager.js
@@ -18,3 +18,27 @@ export const createEvent = (event) => {
 	})
 		.then(response => response.json())
 }
+
+export const getSingleEvent = (id) => {
+	return fetch(`http://localhost:8000/events/${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Token ${localStorage.getItem('lu_token')}`,
+		}
+	})
+		.then(response => response.json())
+}
+
+export const updateEvent = (event, eventId) => {
+	return fetch(`http://localhost:8000/events/${eventId}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+			'Authorization': `Token ${localStorage.getItem('lu_token')}`,
+		},
+		body: JSON.stringify(event)
+	})
+		.then(response => response.json())
+}
